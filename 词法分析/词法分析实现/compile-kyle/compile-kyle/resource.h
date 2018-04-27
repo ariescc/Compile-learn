ce94db669f63169d9f42b6fa2786f66b1147a8ed
ariescc <jiachang@mail.nwpu.edu.cn>
2018-04-27 22:37:07 +0800

    公用定义

---
+++ 词法分析/词法分析实现/compile-kyle/compile-kyle/resource.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "relation.h"
+#include "result.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+
+// 定义使用常量
+
+#define LETTER_NUM 26
+#define DIGIT_NUM 10
+
+
+// 定义状态机返回ID
+
+#define VARIABLE_STATEMACHINE 1000 // 变量赋值状态机
+#define HEADFILE_STATEMACHINE 1001 // 头文件宏定义状态机
+
+
