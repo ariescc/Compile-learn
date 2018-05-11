10712fd03c0e3e0d1a25389c2066c986d14ca7b3
ariescc <jiachang@mail.nwpu.edu.cn>
2018-05-11 19:12:02 +0800

    资源文件

---
+++ exs1/resource.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "relation.h"
+#include "result.h"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+
+
+
+#define LETTER_NUM 26
+#define DIGIT_NUM 10
+
+
+#define VARIABLE_STATEMACHINE 1000
+#define HEADFILE_STATEMACHINE 1001
+#define IDENTIFIER_STATEMACHINE 1002
+#define DECIMAL_STATEMACHINE 1003
+#define KEYWORDS_STATEMACHINE 1004
+#define SYMBOLS_STATEMACHINE 1005
+
+
