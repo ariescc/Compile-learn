752a04a649c5ad51f740d518912c491d3a3c0bc2
ariescc <jiachang@mail.nwpu.edu.cn>
2018-05-24 16:40:14 +0800

    treeNode 定义修改

--- KyleCompiler/TreeNode.h
+++ KyleCompiler/TreeNode.h
@@ -2,13 +2,13 @@
     语法分析树的节点
 */
 
-#define MAXCHILD 50
+#define MAXCHILD 5
 
 struct treeNode {
     struct treeNode *child[MAXCHILD]; // 定义子节点
     struct treeNode *brother; // 兄弟节点指针
 
-    std::string data;
+    std::string data = "null";
 
     int line_no; // 行号
 };
